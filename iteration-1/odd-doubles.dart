b5d4d751a33284023e984f344ce6bb5931927b2a
rchavarria <txingo@gmail.com>
2014-10-31 01:05:50 +0100

    Green

--- iteration-1/odd-doubles.dart
+++ iteration-1/odd-doubles.dart
@@ -1,16 +1,21 @@
 import "package:unittest/unittest.dart";
 
 void main() {
-    
-    group('OddDoubledList', () {
+    group('OddDoubler', () {
         
         test('returns empty list when a == b and both are even', () {
             OddDoubler od = new OddDoubler();
-            List<int> list = od.compute(1, 1);
+            List<int> list = od.compute(2, 2);
 
             expect(list.length, equals(0));
         });
 
     });
+}
+
+class OddDoubler {
+    List<int> compute(int a, int b) {
+        return new List<int>();
+    }
 }
 
