6f2b42d6aae639934ec99227aa6967d4cdb29fe2
rchavarria <txingo@gmail.com>
2014-10-31 01:08:11 +0100

    Red: it returns a list with 1 element if a = b - 1

--- iteration-1/odd-doubles.dart
+++ iteration-1/odd-doubles.dart
@@ -10,6 +10,13 @@ void main() {
             expect(list.length, equals(0));
         });
 
+        test('returns list with 1 element if a == b - 1', () {
+            OddDoubler od = new OddDoubler();
+            List<int> list = od.compute(1, 2);
+
+            expect(list.length, equals(1));
+        });
+
     });
 }
 
