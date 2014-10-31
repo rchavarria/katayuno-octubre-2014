e15e76cadef32da59edb477e125256354232911f
rchavarria <txingo@gmail.com>
2014-10-31 01:13:31 +0100

    Refactor: use one liners for test bodys

--- iteration-1/odd-doubles.dart
+++ iteration-1/odd-doubles.dart
@@ -10,17 +10,11 @@ void main() {
         });
         
         test('returns empty list when a == b and both are even', () {
-            OddDoubler od = new OddDoubler();
-            List<int> list = od.compute(2, 2);
-
-            expect(list.length, equals(0));
+            expect(od.compute(2, 2).length, equals(0));
         });
 
         test('returns list with 1 element if a == b - 1', () {
-            OddDoubler od = new OddDoubler();
-            List<int> list = od.compute(1, 2);
-
-            expect(list.length, equals(1));
+            expect(od.compute(1, 2).length, equals(1));
         });
 
     });
