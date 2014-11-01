d66ed9c44f43c71ce1dac80b7cd3d02aa2578d74
rchavarria <txingo@gmail.com>
2014-11-01 18:57:56 +0100

    Start iteration 2

---
+++ iteration-2/odd-doubles.dart
@@ -0,0 +1,17 @@
+import "package:unittest/unittest.dart";
+
+void main() {
+
+    group('OddDoubler', () {
+        OddDoubler od;
+        
+        setUp(() {
+            od = new OddDoubler();
+        });
+        
+        test('', () {
+        });
+
+    });
+}
+
